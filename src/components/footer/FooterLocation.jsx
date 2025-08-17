9d0001db66224d5e7ded3c4c9b412c0e90bd32fc
MasRizkiii <mrizki.ramadhaniii24@gmail.com>
2025-08-18 05:30:12 +0700

    first commit

--- src/components/footer/FooterLocation.jsx
+++ src/components/footer/FooterLocation.jsx
@@ -4,7 +4,7 @@ export default function FooterLocation() {
       <h3 className="text-lg font-bold mb-3">Lokasi Kami</h3>
       <iframe
         title="Lokasi DKK"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3950.5076976984694!2d110.41005777499778!3d-6.988569493016372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708b4c6e3cf0c3%3A0x8b8e3b8c672db9e6!2sDinas%20Kesehatan%20Kota%20Semarang!5e0!3m2!1sid!2sid!4v1692086400000!5m2!1sid!2sid"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.1899561344608!2d110.41229177399825!3d-6.986891993014063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708b5ab6b34b67%3A0x4eba79d618667033!2sDinas%20Kesehatan%20Kota%20Semarang!5e0!3m2!1sid!2sid!4v1755394793388!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
         width="100%"
         height="150"
         style={{ border: 0 }}
